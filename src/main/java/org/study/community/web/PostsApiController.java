b60ba78d6824e10109e26f7169cbcfcd8b89daf9
bpuuqd <bpuuqd@gmail.com>
2021-05-17 21:55:41 +0900

    social login

---
+++ src/main/java/org/study/community/web/PostsApiController.java
@@ -0,0 +1,35 @@
+package org.study.community.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.study.community.service.PostsService;
+import org.study.community.web.dto.PostsResponseDto;
+import org.study.community.web.dto.PostsSaveRequestDto;
+import org.study.community.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+}
