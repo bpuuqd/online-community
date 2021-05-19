db21bf6c72788d074e5e8e1dc396f3d8b127cacb
bpuuqd <bpuuqd@gmail.com>
2021-05-19 20:59:00 +0900

    switch.sh nginx명령어 오류

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,9 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  echo "response : $RESPONSE"
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
-  echo "upcnt : $UP_COUNT"
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
